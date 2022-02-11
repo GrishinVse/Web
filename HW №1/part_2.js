9686f7d1086d955f8dd54752f4479390322a8838
GrishinVse <vsevolod.grishin01@gmail.com>
2022-02-11 17:37:38 +0300

    Clear

--- HW №1/part_2.js
+++
@@ -1,8 +0,0 @@
-var num_1 = document.getElementById("num_1").value
-var num_2 = document.getElementById("num_2").value
-
-function swapNumbers() {
-    alert("YOU PRESS THE BUTTON")
-}
-
-swapNumbers()
