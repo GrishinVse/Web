bc4eea80a51be030962c3e2b473c39e200256fa6
GrishinVse <vsevolod.grishin01@gmail.com>
2022-03-14 12:22:48 +0300

    New page
    Add new page to search client info by his email.

--- Lab 2/backend/server.js
+++ Lab 2/backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const MongoClient = require("mongodb").MongoClient;
 const { futimesSync } = require('fs');
 const app = express();
 const port = 3000;
@@ -20,7 +21,7 @@ function parseJson(json) {
     let score = 0;
 
     /* Пол */
-    if (json.gender = "female") {
+    if (json.gender == "female") {
         score += 0.4;
     } else {
         score += 0;
@@ -73,21 +74,48 @@ function parseJson(json) {
     }
 
     /* Работа */
-    if (json.sphere = "public") {
+    if (json.sphere == "public") {
         score += 0.21;
     }
 
     /* Занятость */
     score += json.periodWork * 0.059
 
-    console.log("SCORE = ", score)
-
     return score;
 }
 
 app.post('/scoring', (req, res) => {
+
+    const url = "mongodb://localhost:3001/";
+    const client = new MongoClient(url);
+
+    client.connect(function(err, client) {
+        const db = client.db('clients');
+        const collection = db.collection('info');
+        let clientInformation = req.body;
+        collection.insertOne(clientInformation, function(err, result) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(result);
+            console.log(clientInformation);
+            client.close();
+        });
+
+        /*
+        db.command({ ping: 1 }, function(err, result) {
+            if (!err) {
+                console.log("Подключение успешно уcтановлено!");
+                console.log(result);
+            }
+            client.close();
+            console.log("Подключение закрыто");
+        });
+        */
+    });
+
     let client_score = parseJson(req.body);
-    console.log(client_score)
+    console.log("CLIENT SCORE = ", client_score);
     if (client_score > 1.25) {
         res.send('Вам одобрен кредит!');
     } else {
@@ -99,3 +127,32 @@ app.post('/scoring', (req, res) => {
 server.listen(port, function() {
     console.log(`Listening on : ${port}`);
 })
+
+// Операция поиска информации клиента по его почте
+app.post('/search', function(req, res) {
+
+    const url = "mongodb://localhost:3001/";
+    const client = new MongoClient(url);
+
+    let searchEmail = req.body.email;
+    let clientInfoSearch = "";
+
+    client.connect(function(err, client) {
+        const db = client.db('clients');
+        const collection = db.collection('info');
+
+        collection.findOne({ "email": searchEmail }, { _id: 0 }, function(err, result) {
+            if (err) throw err;
+            clientInfoSearch = JSON.stringify(result)
+            res.send(`
+            <form>
+                <label>Ответ от сервера:</label>
+                <p></p>
+                <textarea style="width:350px; height:150px; resize: none;">${clientInfoSearch}</textarea>
+            </form>`)
+            client.close();
+        })
+    });
+})
+
+/*Запуск БД .\mongod --port=3001 --dbpath="D:\_ВУЗ\6 Семестр\Технологии разработки web приложений\Lab 2\data" */
