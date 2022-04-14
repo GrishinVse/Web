bfe7a23a75d9ecd3b06af080813f94a4f1eb7ab8
GrishinVse <vsevolod.grishin01@gmail.com>
2022-04-14 17:22:20 +0300

    Update Lab 3

--- Lab_3/index.js
+++ Lab_3/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const axios = require('axios')
+const axios = require('axios');
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient
+
 const app = express()
 const port = 3000;
 const server = require('http').createServer(app)
@@ -11,36 +14,73 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-    res.sendFile('D:/ВУЗ/6_Семестр/Технологии разработки web приложений/Lab_3/index.html')
+    res.sendFile(path.resolve(__dirname) + '/index.html')
 })
 
 app.get('/get/:val', (req, res) => {
     // Проверка на загруженные курсы
-    let options = {
-        method: 'get',
-        uri: 'https://www.cbr-xml-daily.ru/daily_json.js',
-        json: true
-    }
+    const mondgo_url = 'mongodb://localhost:3001'
+    const clientMongo = new MongoClient(mondgo_url)
 
-    let val = req.params.val
-    let response = null;
-    new Promise(async(resolve, reject) => {
-        try {
-            response = await axios("https://www.cbr-xml-daily.ru/daily_json.js")
-        } catch (er) {
-            response = null;
-            reject(er);
-        }
-        if (response) {
-            let json = response.data;
-            let value = json['Valute'][val]['Value']
-            console.log(value);
-            resolve(json);
-            res.send({ 'value': value });
-        }
+    clientMongo.connect(function(err, client) {
+        if (err) console.log(err);
+
+        const db = client.db('currencies')
+        const collection = db.collection('info');
+        let current_date = new Date().toISOString().slice(0, 10)
+
+        collection.find({ Date: { $regex: `^${current_date}` } }).toArray(function(err, result) {
+            if (err) console.log(err);
+            client.close();
+            let val = req.params.val
+            if (result.length == 0) {
+                let options = {
+                    method: 'get',
+                    uri: 'https://www.cbr-xml-daily.ru/daily_json.js',
+                    json: true
+                }
+                let response = null;
+                new Promise(async(resolve, reject) => {
+                    try {
+                        response = await axios("https://www.cbr-xml-daily.ru/daily_json.js")
+                    } catch (er) {
+                        response = null;
+                        reject(er);
+                    }
+                    if (response) {
+                        let json = response.data;
+
+                        const mondgo_url = 'mongodb://localhost:3001'
+                        const clientMongo = new MongoClient(mondgo_url)
+
+                        clientMongo.connect(function(err, client) {
+                            if (err) console.log(err);
+
+                            const db = client.db('currencies')
+                            const collection = db.collection('info');
+                            collection.insertOne(json, function(err, result) {
+                                if (err) console.log(err);
+                                client.close()
+                            })
+                        })
+
+                        let value = json['Valute'][val]['Value']
+                        console.log("Нет в БД " + value);
+                        res.send({ 'value': value });
+                    }
+                })
+            } else {
+                let json = result[0]
+                let value = json['Valute'][val]['Value']
+                console.log("Есть в БД " + value);
+                res.send({ 'value': value });
+            }
+        })
     });
 });
 
 server.listen(port, function() {
     console.log(`Listening on port ${port}`)
 });
+
+/*Запуск БД .\mongod --port=3001 --dbpath="D:\ВУЗ\6_Семестр\Технологии разработки web приложений\data" */
